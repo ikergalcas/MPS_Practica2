92ed2842989f06a8bab28680df83c0c4a3fe6728
Iker Gálvez Castillo <ikergalcas@gmail.com>
2023-03-07 11:42:14 +0100

    Elaboracion DoubleLinkedListDeque

--- src/main/java/org/mps/deque/DoublyLinkedListDeque.java
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -6,45 +6,55 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
     private DequeNode<T> last;
     private int size;
 
-    public DoublyLinkedListDeque() {
-        // TODO
+    public DoublyLinkedListDeque(DequeNode<T> f, DequeNode<T> l, int s) {
+        first = f;
+        last = l;
+        size = s;
     }
 
     @Override
     public void prepend(T value) {
-        // TODO
+        DequeNode<T> node = new DequeNode<>(value,null, first);
+        first.setPrevious(node);
+        first = node;
+        size++;
     }
 
     @Override
     public void append(T value) {
-        // TODO
+        DequeNode<T> node = new DequeNode<>(value,last, null);
+        last.setNext(node);
+        last = node;
+        size++;
     }
 
     @Override
     public void deleteFirst() {
-        // TODO
+        first = first.getNext();
+        first.setPrevious(null);
+        size--;
     }
 
     @Override
     public void deleteLast() {
         // TODO
+        last = last.getPrevious();
+        last.setNext(null);
+        size--;
     }
 
     @Override
     public T first() {
-        // TODO
-        return null;
+        return first.getItem();
     }
 
     @Override
     public T last() {
-        // TODO
-        return null;
+        return last.getItem();
     }
 
     @Override
     public int size() {
-        // TODO
-        return 0;
+        return size;
     }
 }
